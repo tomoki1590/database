9a0877829c824e4e27b3ada80dda6a4e13c0a931
tomoki1590 <kaguratomato1@gmail.com>
2021-08-13 21:25:35 +0900

    syuuskei

--- lib/screens/test_screen.dart
+++ lib/screens/test_screen.dart
@@ -106,7 +106,7 @@ class _TestScreenState extends State<TestScreen> {
   Widget _isMemorizedCheck() {
     return CheckboxListTile(
         title: Text(
-          "暗記済みの場合はチェックを入れてくださいjjjjjjjj",
+          "暗記済みの場合はチェックを入れてください",
           style: TextStyle(fontSize: 15.0),
         ),
         value: _isMemorized,
